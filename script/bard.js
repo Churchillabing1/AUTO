33f8e1a551bd464cd0c75622abf0ee3724ad9cb8
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-03-28 20:00:24 +0800

    Update bard.js

--- script/bard.js
+++ script/bard.js
@@ -1,35 +1,39 @@
 module.exports.config = {
-  name: "gemini",
-  hasPermission: 0,
-  credits: "Deku", //https://facebook.com/joshg101
-  description: "Talk to Gemini (conversational)",
-  usePrefix: false,
-  commandCategory: "bard",
-  version: "5.6.7"
+    name: "gemini",
+    hasPermission: 0,
+    credits: "Deku", //https://facebook.com/joshg101
+    description: "Talk to Gemini (conversational)",
+    usePrefix: false,
+    commandCategory: "bard",
+    version: "5.6.7"
 };
 
 module.exports.run = async function ({ api, event, args }) {
-	const axios = require("axios");
-	let prompt = args.join(" "),
-		uid = event.senderID,
-		url;
-	if (!prompt) return api.sendMessage(`Please enter a prompt.`, event.threadID);
-	api.sendTypingIndicator(event.threadID);
-	try {
-		const geminiApi = `https://gemini-api.replit.app`;
-		if (event.type == "message_reply") {
-			if (event.messageReply.attachments[0]?.type == "photo") {
-				url = encodeURIComponent(event.messageReply.attachments[0].url);
-				const res = (await axios.get(`${geminiApi}/gemini?prompt=${prompt}&url=${url}&uid=${uid}`)).data;
-				return api.sendMessage(res.gemini, event.threadID);
-			} else {
-				return api.sendMessage('Please reply to an image.', event.threadID);
-			}
-		}
-		const response = (await axios.get(`${geminiApi}/gemini?prompt=${prompt}&uid=${uid}`)).data;
-		return api.sendMessage(response.gemini, event.threadID);
-	} catch (error) {
-		console.error(error);
-		return api.sendMessage('❌ | An error occurred. You can try typing your query again or resending it. There might be an issue with the server that\'s causing the problem, and it might resolve on retrying.', event.threadID);
-	}
+    const axios = require("axios");
+    let prompt = args.join(" "),
+        uid = event.senderID,
+        url;
+    if (!prompt) return api.sendMessage(`Please enter a prompt.`, event.threadID);
+    api.sendTypingIndicator(event.threadID);
+    try {
+        const geminiApi = `https://gemini-api.replit.app`;
+        if (event.type == "message_reply") {
+            if (event.messageReply.attachments[0]?.type == "photo") {
+                url = encodeURIComponent(event.messageReply.attachments[0].url);
+                const res = (await axios.get(`${geminiApi}/gemini?prompt=${prompt}&url=${url}&uid=${uid}`)).data;
+                let response = res.gemini;
+                response += `\n\nAnswer provided by: Churchill\nFacebook: https://www.facebook.com/profile.php?id=100087212564100`;
+                return api.sendMessage(response, event.threadID);
+            } else {
+                return api.sendMessage('Please reply to an image.', event.threadID);
+            }
+        }
+        const response = (await axios.get(`${geminiApi}/gemini?prompt=${prompt}&uid=${uid}`)).data;
+        let geminiResponse = response.gemini;
+        geminiResponse += `\n\nAnswer provided by: Churchill\nFacebook: https://www.facebook.com/profile.php?id=100087212564100`;
+        return api.sendMessage(geminiResponse, event.threadID);
+    } catch (error) {
+        console.error(error);
+        return api.sendMessage('❌ | An error occurred. You can try typing your query again or resending it. There might be an issue with the server that\'s causing the problem, and it might resolve on retrying.', event.threadID);
+    }
 };
