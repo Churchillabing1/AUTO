e03750af06566dda4d19b373c922c04f5515a08f
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-29 09:14:15 +0800

    improve

--- fb-chat-api/src/changeAvatar.js
+++ fb-chat-api/src/changeAvatar.js
@@ -4,8 +4,11 @@ var utils = require("../utils");
 var log = require("npmlog");
 var bluebird = require("bluebird");
 
+// Promisify the postFormData function
+var postFormDataPromisified = bluebird.promisify(defaultFuncs.postFormData);
+
 module.exports = function (defaultFuncs, api, ctx) {
-  function handleUpload(image, callback) {
+  function handleUpload(image) {
     var uploads = [];
 
     var form = {
@@ -16,112 +19,83 @@ module.exports = function (defaultFuncs, api, ctx) {
     };
 
     uploads.push(
-      defaultFuncs
-        .postFormData(
-          "https://www.facebook.com/profile/picture/upload/",
-          ctx.jar,
-          form,
-          {}
-        )
-        .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-        .then(function (resData) {
-          if (resData.error) {
-            throw resData;
-          }
-          return resData;
-        })
+      postFormDataPromisified(
+        "https://www.facebook.com/profile/picture/upload/",
+        ctx.jar,
+        form,
+        {}
+      )
+      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+      .then(function (resData) {
+        if (resData.error) {
+          throw resData;
+        }
+        return resData;
+      })
     );
 
     // resolve all promises
-    bluebird
-      .all(uploads)
-      .then(function (resData) {
-        callback(null, resData);
-      })
-      .catch(function (err) {
-        log.error("handleUpload", err);
-        return callback(err);
-      });
+    return bluebird.all(uploads);
   }
 
-  return function changeAvatar(image, caption = "", timestamp = null, callback) {
-    var resolveFunc = function () { };
-    var rejectFunc = function () { };
-    var returnPromise = new Promise(function (resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
-
-    if (!timestamp && utils.getType(caption) === "Number") {
-      timestamp = caption;
-      caption = "";
-    }
-
-    if (!timestamp && !callback && (utils.getType(caption) == "Function" || utils.getType(caption) == "AsyncFunction")) {
-      callback = caption;
-      caption = "";
-      timestamp = null;
-    }
-
-    if (!callback) callback = function (err, data) {
-      if (err) {
-        return rejectFunc(err);
-      }
-      resolveFunc(data);
-    };
-
-    if (!utils.isReadableStream(image))
-      return callback("Image is not a readable stream");
-
-    handleUpload(image, function (err, payload) {
-      if (err) {
-        return callback(err);
+  function changeAvatar(image, caption = "", timestamp = null) {
+    return new bluebird(function (resolve, reject) {
+      if (!utils.isReadableStream(image)) {
+        return reject("Image is not a readable stream");
       }
 
-      var form = {
-        av: ctx.userID,
-        fb_api_req_friendly_name: "ProfileCometProfilePictureSetMutation",
-        fb_api_caller_class: "RelayModern",
-        doc_id: "5066134240065849",
-        variables: JSON.stringify({
-          input: {
-            caption,
-            existing_photo_id: payload[0].payload.fbid,
-            expiration_time: timestamp,
-            profile_id: ctx.userID,
-            profile_pic_method: "EXISTING",
-            profile_pic_source: "TIMELINE",
-            scaled_crop_rect: {
-              height: 1,
-              width: 1,
-              x: 0,
-              y: 0
-            },
-            skip_cropping: true,
-            actor_id: ctx.userID,
-            client_mutation_id: Math.round(Math.random() * 19).toString()
-          },
-          isPage: false,
-          isProfile: true,
-          scale: 3
+      if (!ctx.jar || !defaultFuncs.postFormData) {
+        return reject("Invalid context or defaultFuncs");
+      }
+
+      handleUpload(image)
+        .then(function (payload) {
+          var form = {
+            av: ctx.userID,
+            fb_api_req_friendly_name: "ProfileCometProfilePictureSetMutation",
+            fb_api_caller_class: "RelayModern",
+            doc_id: "5066134240065849",
+            variables: JSON.stringify({
+              input: {
+                caption,
+                existing_photo_id: payload[0].payload.fbid,
+                expiration_time: timestamp,
+                profile_id: ctx.userID,
+                profile_pic_method: "EXISTING",
+                profile_pic_source: "TIMELINE",
+                scaled_crop_rect: {
+                  height: 1,
+                  width: 1,
+                  x: 0,
+                  y: 0
+                },
+                skip_cropping: true,
+                actor_id: ctx.userID,
+                client_mutation_id: Math.round(Math.random() * 19).toString()
+              },
+              isPage: false,
+              isProfile: true,
+              scale: 3
+            })
+          };
+
+          return defaultFuncs
+            .post("https://www.facebook.com/api/graphql/", ctx.jar, form)
+            .then(utils.parseAndCheckLogin(ctx, defaultFuncs));
         })
-      };
-
-      defaultFuncs
-        .post("https://www.facebook.com/api/graphql/", ctx.jar, form)
-        .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
         .then(function (resData) {
           if (resData.errors) {
             throw resData;
           }
-          return callback(null, resData[0].data.profile_picture_set);
+          return resolve(resData[0].data.profile_picture_set);
         })
         .catch(function (err) {
           log.error("changeAvatar", err);
-          return callback(err);
+          return reject(err);
         });
     });
+  }
 
-    return returnPromise;
-  };
+  return changeAvatar;
 };
+
