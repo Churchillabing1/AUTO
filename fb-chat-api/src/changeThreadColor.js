665302fe04079fa83a72e967d5e937172b3d2259
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-29 09:14:16 +0800

    improve

--- fb-chat-api/src/changeThreadColor.js
+++ fb-chat-api/src/changeThreadColor.js
@@ -1,32 +1,23 @@
 "use strict";
 
-var utils = require("../utils");
-var log = require("npmlog");
+const utils = require("../utils");
+const log = require("npmlog");
 
 module.exports = function (defaultFuncs, api, ctx) {
   return function changeThreadColor(color, threadID, callback) {
-    var resolveFunc = function () { };
-    var rejectFunc = function () { };
-    var returnPromise = new Promise(function (resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
-
-    if (!callback) {
-      callback = function (err) {
-        if (err) {
-          return rejectFunc(err);
-        }
-        resolveFunc(err);
-      };
+    // Validate color input
+    if (color !== null && !/^#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/.test(color)) {
+      return callback(new Error("Invalid color input. Please provide a valid hex color code."));
     }
 
-    if (!isNaN(color)) {
-      color = color.toString();
+    // Validate threadID input
+    if (typeof threadID !== "string" || threadID.trim() === "") {
+      return callback(new Error("Invalid threadID input. Please provide a valid threadID."));
     }
-    var validatedColor = color !== null ? color.toLowerCase() : color; // API only accepts lowercase letters in hex string
 
-    var form = {
+    const validatedColor = color !== null ? color.toLowerCase() : color; // API only accepts lowercase letters in hex string
+
+    const form = {
       dpr: 1,
       queries: JSON.stringify({
         o0: {
@@ -45,21 +36,36 @@ module.exports = function (defaultFuncs, api, ctx) {
       })
     };
 
-    defaultFuncs
-      .post("https://www.facebook.com/api/graphqlbatch/", ctx.jar, form)
-      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function (resData) {
-        if (resData[resData.length - 1].error_results > 0) {
-          throw resData[0].o0.errors;
-        }
+    // Create a new promise
+    const returnPromise = new Promise((resolve, reject) => {
+      defaultFuncs
+        .post("https://www.facebook.com/api/graphqlbatch/", ctx.jar, form)
+        .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+        .then(resData => {
+          if (resData[resData.length - 1].error_results > 0) {
+            throw resData[0].o0.errors;
+          }
 
-        return callback();
-      })
-      .catch(function (err) {
-        log.error("changeThreadColor", err);
-        return callback(err);
-      });
+          // Resolve the promise if no error
+          resolve();
+        })
+        .catch(err => {
+          log.error("changeThreadColor", err);
+          // Reject the promise with the error
+          reject(err);
+        });
+    });
 
-    return returnPromise;
+    // If no callback is provided, use the promise
+    if (!callback) {
+      return returnPromise;
+    }
+
+    // Attach the callback to the promise
+    returnPromise.then(() => {
+      callback(null);
+    }).catch(err => {
+      callback(err);
+    });
   };
 };
