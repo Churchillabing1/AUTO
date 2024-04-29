51863f53d87151d1662013d00d727bc903ec09e1
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-29 09:14:19 +0800

    improve

--- script/event/resend.js
+++ script/event/resend.js
@@ -1,57 +1,88 @@
+const fs = require('fs');
+const axios = require('axios');
+
 module.exports.config = {
     name: "resend",
     version: "1.0.0",
-    
 };
-var msgData = {} 
+
+const msgData = {};
 
 module.exports.handleEvent = async function ({ api, event }) {
-  if(event.type == 'message') {
-    msgData[event.messageID] = {
-      body: event.body, 
-      attachments: event.attachments
+    if (event.type == 'message') {
+        msgData[event.messageID] = {
+            body: event.body,
+            attachments: event.attachments
+        }
     }
 
-  }
-  if(event.type == "message_unsend" && msgData.hasOwnProperty(event.messageID)) { 
-const info = await api.getUserInfo(event.senderID);
-const name = info[event.senderID].name
-    const axios = require('axios');
-    const fs = require("fs")
-    if(msgData[event.messageID].attachments.length === 0) {
-        api.sendMessage(`${name} unsent this message: ${msgData[event.messageID].body}`, event.threadID)  
-    } else if(msgData[event.messageID].attachments[0].type == 'photo')  {   
-      var photo = []
-      var del = []
-      for (const item of msgData[event.messageID].attachments) {
-       let { data } = await axios.get(item.url, {responseType: "arraybuffer"})
-          fs.writeFileSync(`./script/cache/${item.filename}.jpg`, Buffer.from(data))
-          photo.push(fs.createReadStream(`./script/cache/${item.filename}.jpg`))
-          del.push(`./script/cache/${item.filename}.jpg`)
-      }
-                    api.sendMessage({body:`${name} unsent this photo: ${msgData[event.messageID].body}`, attachment: photo}, event.threadID, () => {
-               for (const item of del) fs.unlinkSync(item)
-             }) 
+    if (event.type == "message_unsend" && msgData.hasOwnProperty(event.messageID)) {
+        const userInfo = await api.getUserInfo(event.senderID);
+        const senderName = userInfo.name;
 
-} else if (msgData[event.messageID].attachments[0].type == 'audio') { 
-
-let { data } = await axios.get(msgData[event.messageID].attachments[0].url, {responseType: "arraybuffer"})
-
- fs.writeFileSync(`./script/cache/audio.mp3`, Buffer.from(data)) 
-
-api.sendMessage({body:`${name} unsent this voice message: ${msgData[event.messageID].body}`, attachment: fs.createReadStream('./script/cache/audio.mp3')}, event.threadID, () => {
-     fs.unlinkSync('./script/cache/audio.mp3')
-             });
-
-    } else if (msgData[event.messageID].attachments[0].type == 'animated_image') {
-
- let { data } = await axios.get(msgData[event.messageID].attachments[0].previewUrl, {responseType: "arraybuffer"})
-
- fs.writeFileSync(`./script/cache/animated_image.gif`, Buffer.from(data)) 
-
-api.sendMessage({body:`${name} unsent this gif: ${msgData[event.messageID].body}`, attachment: fs.createReadStream('./script/cache/animated_image.gif')}, event.threadID, () => {
-     fs.unlinkSync('./script/cache/animated_image.gif')
-             });     
+        if (msgData[event.messageID].attachments.length === 0) {
+            api.sendMessage(`${senderName} unsent this message: ${msgData[event.messageID].body}`, event.threadID);
+        } else {
+            const attachment = getAttachment(msgData[event.messageID].attachments[0]);
+            if (attachment) {
+                api.sendMessage({ body: `${senderName} unsent this ${attachment.type}: ${msgData[event.messageID].body}`, attachment: attachment.stream }, event.threadID, () => {
+                    if (attachment.deleteFile) {
+                        fs.unlinkSync(attachment.filePath);
+                    }
+                });
+            }
+        }
     }
-  }
+};
+
+function getAttachment(attachment) {
+    switch (attachment.type) {
+        case 'photo':
+            const photo = [];
+            const photoFiles = [];
+            for (const item of attachment.attachments) {
+                axios.get(item.url, { responseType: "arraybuffer" })
+                    .then(response => {
+                        const filePath = `./script/cache/${item.filename}.jpg`;
+                        fs.writeFileSync(filePath, Buffer.from(response.data));
+                        photo.push(fs.createReadStream(filePath));
+                        photoFiles.push(filePath);
+                    })
+                    .catch(error => {
+                        console.error(`Error downloading photo: ${error}`);
+                    });
+            }
+
+            if (photo.length > 0) {
+                return { type: 'photo', stream: photo, deleteFile: true, filePath: photoFiles };
+            }
+            break;
+
+        case 'audio':
+            axios.get(attachment.url, { responseType: "arraybuffer" })
+                .then(response => {
+                    const filePath = `./script/cache/audio.mp3`;
+                    fs.writeFileSync(filePath, Buffer.from(response.data));
+                })
+                .catch(error => {
+                    console.error(`Error downloading audio: ${error}`);
+                });
+
+            return { type: 'audio', stream: fs.createReadStream('./script/cache/audio.mp3'), deleteFile: true, filePath: './script/cache/audio.mp3' };
+
+        case 'animated_image':
+            axios.get(attachment.previewUrl, { responseType: "arraybuffer" })
+                .then(response => {
+                    const filePath = `./script/cache/animated_image.gif`;
+                    fs.writeFileSync(filePath, Buffer.from(response.data));
+                })
+                .catch(error => {
+                    console.error(`Error downloading animated image: ${error}`);
+                });
+
+            return { type: 'animated_image', stream: fs.createReadStream('./script/cache/animated_image.gif'), deleteFile: true, filePath: './script/cache/animated_image.gif' };
+
+        default:
+            return null;
+    }
 }
